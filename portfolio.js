237ecc00ac6c8e8849ecaff8d5ac22afa9865504
Alan Campbell <alancampbell4444@gmail.com>
2016-11-09 21:07:32 -0800

    jquery progress

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,25 @@
 $(function(){
-  $('h1').mouseenter(function(){
+  
+  $('h1').hover(function(){                 //h1
   	$(this).css({'color':'green'})});
   $('h1').mouseleave(function(){
-  	$(this).css({'color':'black'})});
+  	$(this).css({'color':'black'})});       //h1
+ 
+ $('.badges').find('ul').hide();
+ 
+ $('button').click(function(){                 //h1
+  	$(this).closest('.badges').find('ul').toggle();
+  	if(butt === 'off') {
+      $('.butt').text('Click to See My Badges');
+      butt = 'on';
+    }else {
+       $('.butt').text('Hide My Badges');
+      butt = 'off';
+    }
+
+  });
+   
+ 
 });
+var butt = "on"
+
